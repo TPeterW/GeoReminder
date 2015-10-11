99b3e05e0af86897774e005e7a7d8d7bc1b1becb
Tao Peter Wang <peterwangtao0@hotmail.com>
2015-10-11 15:07:03 -0400

    Updated RecyclerView
    Fixed some UI bugs

--- app/src/main/java/com/peter/georeminder/SplashScreen.java
+++ app/src/main/java/com/peter/georeminder/SplashScreen.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 
 /**
  * Created by Peter on 10/11/15.
@@ -15,6 +16,11 @@ public class SplashScreen extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
 
         new Handler().postDelayed(new Runnable() {
 
